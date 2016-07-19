9d52f996b84fb6d1021b5aa1be24fc28d072f4a4
dereklholder <dereklholder@gmail.com>
2016-07-19 08:23:49 -0600

    Removed Dependency, Manually added Code to Project.

---
+++ HPF eComm Demo/OEHP/GatewayRequest.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.IO;
+using System.Collections.Specialized;
+using Newtonsoft.Json;
+
+namespace HostPayFunctions
+{
+    public class GatewayRequest : VariableHandler
+    {
+
+        //Posts to TEST
+        public string TestPayPagePost(string parameters) //Returns url that will render paypage.
+        {
+            try
+            {
+                WebRequest oehpRequest = WebRequest.Create(TestPayPagePostURL);
+
+                string postData = parameters;
+                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+                oehpRequest.ContentType = "application/x-www-form-urlencoded";
+                oehpRequest.Method = "POST";
+                oehpRequest.ContentLength = byteArray.Length;
+
+                Stream dataStream = oehpRequest.GetRequestStream();
+
+                dataStream.Write(byteArray, 0, byteArray.Length);
+
+                dataStream.Close();
+
+                WebResponse oehpResponse = oehpRequest.GetResponse();
+
+                dataStream = oehpResponse.GetResponseStream();
+
+                StreamReader reader = new StreamReader(dataStream);
+
+                string responseFromOEHP = reader.ReadToEnd();
+
+                reader.Close();
+                oehpResponse.Close();
+
+                PayPageJson jsonResponse = new PayPageJson();
+                jsonResponse = JsonConvert.DeserializeObject<PayPageJson>(responseFromOEHP);
+
+                string result = jsonResponse.actionUrl + jsonResponse.sealedSetupParameters;
+
+                SessionToken = jsonResponse.sealedSetupParameters;
+
+                //If an error Occured, change the result to the error message
+                if (jsonResponse.errorMessage != null)
+                {
+                    result = jsonResponse.errorMessage;
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+        public string TestDirectPost(string parameters) //Returns raw result from direct OEHP, will need to construct wrapper to handle result data.
+        {
+            try
+            {
+
+                WebRequest oehpRequest = WebRequest.Create(TestDirectPostURL);
+
+                string postData = parameters;
+                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+                oehpRequest.ContentType = "application/x-www-form-urlencoded";
+                oehpRequest.Method = "POST";
+                oehpRequest.ContentLength = byteArray.Length;
+
+                Stream dataStream = oehpRequest.GetRequestStream();
+
+                dataStream.Write(byteArray, 0, byteArray.Length);
+
+                dataStream.Close();
+
+                WebResponse oehpResponse = oehpRequest.GetResponse();
+
+                dataStream = oehpResponse.GetResponseStream();
+
+                StreamReader reader = new StreamReader(dataStream);
+
+                string responseFromOEHP = reader.ReadToEnd();
+
+                reader.Close();
+                oehpResponse.Close();
+
+                return responseFromOEHP;
+
+
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+        public string TestHtmlDocPost(string parameters)  //Performs HTMLDOc Post, returns HTML doc as String
+        {
+            try
+            {
+                WebRequest oehpRequest = WebRequest.Create(TestHtmlDocPostURL);
+
+                string postData = parameters;
+                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+                oehpRequest.ContentType = "application/x-www-form-urlencoded";
+                oehpRequest.Method = "POST";
+                oehpRequest.ContentLength = byteArray.Length;
+
+                Stream dataStream = oehpRequest.GetRequestStream();
+
+                dataStream.Write(byteArray, 0, byteArray.Length);
+
+                dataStream.Close();
+
+                WebResponse oehpResponse = oehpRequest.GetResponse();
+
+                dataStream = oehpResponse.GetResponseStream();
+
+                StreamReader reader = new StreamReader(dataStream);
+
+                string responseFromOEHP = reader.ReadToEnd();
+
+                reader.Close();
+                oehpResponse.Close();
+
+                return responseFromOEHP;
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+
+        }
+        //Posts to PROD
+        public string LivePayPagePost(string parameters)
+        {
+            try
+            {
+                WebRequest oehpRequest = WebRequest.Create(LivePayPagePostURL);
+
+                string postData = parameters;
+                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+                oehpRequest.ContentType = "application/x-www-form-urlencoded";
+                oehpRequest.Method = "POST";
+                oehpRequest.ContentLength = byteArray.Length;
+
+                Stream dataStream = oehpRequest.GetRequestStream();
+
+                dataStream.Write(byteArray, 0, byteArray.Length);
+
+                dataStream.Close();
+
+                WebResponse oehpResponse = oehpRequest.GetResponse();
+
+                dataStream = oehpResponse.GetResponseStream();
+
+                StreamReader reader = new StreamReader(dataStream);
+
+                string responseFromOEHP = reader.ReadToEnd();
+
+                reader.Close();
+                oehpResponse.Close();
+
+                PayPageJson jsonResponse = new PayPageJson();
+                jsonResponse = JsonConvert.DeserializeObject<PayPageJson>(responseFromOEHP);
+
+                SessionToken = jsonResponse.sealedSetupParameters;
+
+                string result = jsonResponse.actionUrl + jsonResponse.sealedSetupParameters;
+
+                //If an error Occured, change the result to the error message
+                if (jsonResponse.errorMessage != null)
+                {
+                    result = jsonResponse.errorMessage;
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+        public string LiveDirectPost(string parameters) //Returns raw result from direct OEHP, will need to construct wrapper to handle result data.
+        {
+            try
+            {
+
+                WebRequest oehpRequest = WebRequest.Create(LiveDirectPostURL);
+
+                string postData = parameters;
+                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+                oehpRequest.ContentType = "application/x-www-form-urlencoded";
+                oehpRequest.Method = "POST";
+                oehpRequest.ContentLength = byteArray.Length;
+
+                Stream dataStream = oehpRequest.GetRequestStream();
+
+                dataStream.Write(byteArray, 0, byteArray.Length);
+
+                dataStream.Close();
+
+                WebResponse oehpResponse = oehpRequest.GetResponse();
+
+                dataStream = oehpResponse.GetResponseStream();
+
+                StreamReader reader = new StreamReader(dataStream);
+
+                string responseFromOEHP = @reader.ReadToEnd();
+
+                reader.Close();
+                oehpResponse.Close();
+
+                return responseFromOEHP;
+
+
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+        public string LiveHtmlDocumentPost(string parameters)  //Not Fully IMplemented
+        {
+            try
+            {
+                WebRequest oehpRequest = WebRequest.Create(LiveHtmlDocPostURL);
+
+                string postData = parameters;
+                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+                oehpRequest.ContentType = "application/x-www-form-urlencoded";
+                oehpRequest.Method = "POST";
+                oehpRequest.ContentLength = byteArray.Length;
+
+                Stream dataStream = oehpRequest.GetRequestStream();
+
+                dataStream.Write(byteArray, 0, byteArray.Length);
+
+                dataStream.Close();
+
+                WebResponse oehpResponse = oehpRequest.GetResponse();
+
+                dataStream = oehpResponse.GetResponseStream();
+
+                StreamReader reader = new StreamReader(dataStream);
+
+                string responseFromOEHP = reader.ReadToEnd();
+
+                reader.Close();
+                oehpResponse.Close();
+
+                return responseFromOEHP;
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+
+        }
+    }
+
+}
---
+++ HPF eComm Demo/OEHP/VariableHandler.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostPayFunctions
+{
+    public class VariableHandler
+    {
+
+
+        //Test OEHP URLs
+        public string TestPayPagePostURL = "https://ws.test.paygateway.com/HostPayService/v1/hostpay/transactions";
+        public string TestDirectPostURL = "https://ws.test.paygateway.com/api/v1/transactions";
+        public string TestHtmlDocPostURL = "https://ws.test.paygateway.com/HostPayService/v1/hostpay/paypage/";
+        public static string TestRcmStatusURL = "https://ws.test.paygateway.com/HostPayService/v1/hostpay/transactions/status/";
+
+        //Live OEHP Urls
+        public string LivePayPagePostURL = "https://ws.paygateway.com/HostPayService/v1/hostpay/transactions";
+        public string LiveDirectPostURL = "https://ws.paygateway.com/api/v1/transactions";
+        public string LiveHtmlDocPostURL = "https://ws.paygateway.com/HostPayService/v1/hostpay/paypage/";
+        public static string LiveRcmStatusURL = "https://ws.paygateway.com/HostPayService/v1/hostpay/transactions/status/";
+
+        //Object for deserializing the JSOn returned from PayPage POST
+        public struct PayPageJson
+        {
+            public string sealedSetupParameters { get; set; }
+            public string actionUrl { get; set; }
+            public string errorMessage { get; set; }
+        }
+        public static string SessionToken { get; set; }
+        //Object for deserializing result of Query transactions.
+        public struct QueryResultJson
+        {
+            //General Transaction data
+            public string response_code { get; set; }
+            public string response_code_text { get; set; }
+            public string secondary_response_code { get; set; }
+            public string secondary_response_code_text { get; set; }
+            public string time_stamp { get; set; }
+            public string retry_recommended { get; set; }
+            public string authorized_amount { get; set; }
+            public string bin { get; set; }
+            public string captured_amount { get; set; }
+            public string original_authorized_amount { get; set; }
+            public string requested_amount { get; set; }
+            public string time_stamp_created { get; set; }
+            public string original_response_code { get; set; }
+            public string original_response_code_text { get; set; }
+            public string original_secondary_response_code { get; set; }
+            public string original_secondary_response_code_text { get; set; }
+            public string time_stamp_updated { get; set; }
+            public string merchant_receipt { get; set; }
+            public string customer_receipt { get; set; }
+            public string state { get; set; }
+            public string bank_approval_code { get; set; }
+            public string expire_month { get; set; }
+            public string expire_year { get; set; }
+            public string order_id { get; set; }
+            public string payer_identifier { get; set; }
+            public string reference_id { get; set; }
+            public string span { get; set; }
+            public string card_brand { get; set; }
+            public string batch_id { get; set; }
+
+            //Receipt Tags
+            public string receipt_application_cryoptogram_label { get; set; }
+            public string receipt_application_cryptogram { get; set; }
+            public string receipt_application_identifier_label { get; set; }
+            public string receipt_application_identifier { get; set; }
+            public string receipt_application_preferred_name { get; set; }
+            public string receipt_application_preferred_name_label { get; set; }
+            public string receipt_application_transaction_counter_label { get; set; }
+            public string receipt_application_transaction_counter { get; set; }
+            public string receipt_approval_code_label { get; set; }
+            public string receipt_approval_code { get; set; }
+            public string receipt_authorization_response_code_label { get; set; }
+            public string receipt_authorization_response_code { get; set; }
+            public string receipt_card_number_label { get; set; }
+            public string receipt_card_number { get; set; }
+            public string receipt_card_type_label { get; set; }
+            public string receipt_card_type { get; set; }
+            public string receipt_entry_legend_label { get; set; }
+            public string receipt_entry_legend { get; set; }
+            public string receipt_entry_method_label { get; set; }
+            public string receipt_entry_method { get; set; }
+            public string receipt_line_items { get; set; }
+            public string receipt_merchant_id_label { get; set; }
+            public string receipt_merchant_id { get; set; }
+            public string receipt_order_id_label { get; set; }
+            public string receipt_order_id { get; set; }
+            public string receipt_signature_text { get; set; }
+            public string receipt_terminal_verification_results_label { get; set; }
+            public string receipt_terminal_verification_results { get; set; }
+            public string receipt_total_amount_label { get; set; }
+            public string receipt_total_amount { get; set; }
+            public string receipt_transaction_date_time_label { get; set; }
+            public string receipt_transaction_date_time { get; set; }
+            public string receipt_transaction_id_label { get; set; }
+            public string receipt_transaction_id { get; set; }
+            public string receipt_transaction_reference_number_label { get; set; }
+            public string receipt_transaction_reference_number { get; set; }
+            public string receipt_transaction_status_information_label { get; set; }
+            public string receipt_transaction_type_label { get; set; }
+            public string receipt_transaction_type { get; set; }
+            public string receipt_validation_code_label { get; set; }
+            public string receipt_validation_code { get; set; }
+            public string receipt_verbiage { get; set; }
+            public string receipt_merchant_copy_label { get; set; }
+            public string receipt_customer_copy_label { get; set; }
+
+            public string exceptionData { get; set; } //If an Exception Occurs when Deserializing, puts exception string here.
+
+
+        }
+    }
+    public static class SSP
+    {
+        public static string SessionToken { get; set; }
+    }
+}
